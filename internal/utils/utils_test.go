a1a03099e6b680864935a2d4766509a6b3d9a9de
a-korkin <a-korkin88@yandex.ru>
2025-01-13 19:56:00 +0300

    test: fixed utils

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,6 +1,8 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGetVars(t *testing.T) {
 	vars := map[string]string{
@@ -9,28 +11,45 @@ func TestGetVars(t *testing.T) {
 	}
 
 	tests := []struct {
-		name string
-		url  string
-		path string
+		name   string
+		url    string
+		path   string
+		fields []string
 	}{
 		{
-			name: "have id",
-			url:  "/products/5ee2371d-3065-4f69-8e3f-d1a31df2ef74",
-			path: "/{id}",
+			name:   "have id",
+			url:    "/products/5ee2371d-3065-4f69-8e3f-d1a31df2ef74",
+			path:   "/{id}",
+			fields: []string{"id"},
 		},
 		{
-			name: "have id and category",
-			url:  "/products/5ee2371d-3065-4f69-8e3f-d1a31df2ef74/91f43fe2-4d8a-4a21-865a-fe9dc49aa3c7",
-			path: "/{id}/{category}",
+			name:   "have id and category",
+			url:    "/products/5ee2371d-3065-4f69-8e3f-d1a31df2ef74/91f43fe2-4d8a-4a21-865a-fe9dc49aa3c7",
+			path:   "/{id}/{category}",
+			fields: []string{"id", "category"},
 		},
+		// {
+		// 	name:   "don't have id",
+		// 	url:    "/products",
+		// 	path:   "/{id}",
+		// 	fields: []string{"id"},
+		// },
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			exp := vars["id"]
 			got := GetVars(tt.url, tt.path)
-			if vars["id"] != got["id"] {
-				t.Errorf("Incorrect result, expected: %s, got: %s", exp, got)
+			for _, field := range tt.fields {
+				exp, ok := vars[field]
+				if ok {
+					if exp != got[field] {
+						t.Errorf("Incorrect result, expected: %s, got: %s", exp, got)
+					}
+				} else {
+					// if exp == got[field] {
+					// 	t.Errorf("Incorrect result, expected: %s, got: %s", exp, got)
+					// }
+				}
 			}
 		})
 	}
