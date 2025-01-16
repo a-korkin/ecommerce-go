c59f88a0a50c30de87411e401ad6bfaee01248f3
a-korkin <a-korkin88@yandex.ru>
2025-01-16 09:13:09 +0300

    test: deleting category

--- internal/web/handlers/category_test.go
+++ internal/web/handlers/category_test.go
@@ -59,7 +59,8 @@ func prepareData() {
 insert into public.categories(id, title, code)
 values
 	('688e64d3-c722-48e5-be96-850e419df2d6', 'category@1', 'cat@1'),
-	('996be659-81f0-457c-8682-800abcfd64c2', 'category@2', 'cat@2');`
+	('996be659-81f0-457c-8682-800abcfd64c2', 'category@2', 'cat@2'),
+	('efa8b389-a3bd-4e06-84dd-4960a0dfc55b', 'category@3', 'cat@3');`
 	if _, err := runner.Connection.DB.Exec(sql); err != nil {
 		log.Fatal(err)
 	}
@@ -123,7 +124,7 @@ func TestGetByID(t *testing.T) {
 
 func TestCreate(t *testing.T) {
 	rr := httptest.NewRecorder()
-	categoryData := []byte(`{"title":"category@3", "code":"cat@3"}`)
+	categoryData := []byte(`{"title":"category@4", "code":"cat@4"}`)
 	req := httptest.NewRequest(http.MethodPost, "/categories",
 		bytes.NewBuffer(categoryData))
 	req.Header.Set("Content-Type", "application/json")
@@ -137,13 +138,12 @@ func TestCreate(t *testing.T) {
 	if err := json.NewDecoder(rr.Body).Decode(&out); err != nil {
 		t.Errorf("Failed to unmarshalling category: %s", err)
 	}
-	if out.Title != "category@3" || out.Code != "cat@3" {
+	if out.Title != "category@4" || out.Code != "cat@4" {
 		t.Errorf("Wrong unmarshalling category, got: %v", out)
 	}
 }
 
 func TestUpdate(t *testing.T) {
-	// ('996be659-81f0-457c-8682-800abcfd64c2', 'category@2', 'cat@2');`
 	id := "996be659-81f0-457c-8682-800abcfd64c2"
 	rr := httptest.NewRecorder()
 	categoryData := []byte(`{"title":"upd title", "code":"upd code"}`)
@@ -161,5 +161,16 @@ func TestUpdate(t *testing.T) {
 	}
 	if out.Title != "upd title" || out.Code != "upd code" {
 		t.Errorf("Wrong unmarshalling category, got: %v", out)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	id := "efa8b389-a3bd-4e06-84dd-4960a0dfc55b"
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodDelete, "/categories", nil)
+	runner.Handler.delete(rr, req, id)
+	if status := rr.Code; status != http.StatusNoContent {
+		t.Errorf("Handler returned wrong status code, got: %v, want: %v",
+			status, http.StatusNoContent)
 	}
 }
