a5335796c01cf9d659f2599ae568cff27ce3c7e4
a-korkin <a-korkin88@yandex.ru>
2025-01-18 10:37:59 +0300

    feat: seed test data

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
+include .env
+
 prepare: 
 	goose up
 run:
 	go run cmd/main.go
 test:
 	go test ./... -v -cover
+seed_data:
+	PGPASSWORD=${PGPASSWORD} psql -h localhost -U postgres -d \
+			   ${DB_NAME} < scripts/seed_categories_and_products.sql
---
+++ scripts/seed_categories_and_products.sql
@@ -0,0 +1,14 @@
+insert into public.categories(title, code)
+select 'category#' || a::text as title, 'cat#' || a::text as code
+from generate_series(1, 100) as a;
+
+insert into public.products(title, category, price)
+select 
+    'product#' || a::text as title, b.id as category,
+    random() * 100 + 20 as price
+from 
+(
+    select a, (random() * 100)::integer as c
+    from generate_series(1, 10000) as a
+) as a
+inner join public.categories as b on b.code = 'cat#' || a.c;
