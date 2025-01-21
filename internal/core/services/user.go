4241ddeb9d26887688e93ee0c935afb23d7f1b07
a-korkin <a-korkin88@yandex.ru>
2025-01-21 09:02:01 +0300

    feat: crud user

--- internal/core/services/user.go
+++ internal/core/services/user.go
@@ -17,7 +17,7 @@ func (s *UserService) Create(in *models.UserIn) (*models.User, error) {
 	sql := `
 insert into public.users(first_name, last_name)
 values($1, $2)
-returning id, last_name, first_name;`
+returning id, last_name, first_name`
 	row, err := s.DB.Query(sql, in.LastName, in.FirstName)
 	if err != nil {
 		return nil, err
@@ -45,4 +45,44 @@ limit $2::integer`
 		return nil, err
 	}
 	return users, nil
+}
+
+func (s *UserService) Update(id string, in *models.UserIn) (*models.User, error) {
+	sql := `
+update public.users
+set last_name = $2,
+	first_name = $3
+where id = $1::uuid
+returning id, last_name, first_name`
+	row, err := s.DB.Query(sql, id, in.LastName, in.FirstName)
+	if err != nil {
+		return nil, err
+	}
+	out := models.User{}
+	if row.Next() {
+		if err = row.Scan(&out.ID, &out.LastName, &out.FirstName); err != nil {
+			return nil, err
+		}
+	}
+	return &out, nil
+}
+
+func (s *UserService) GetByID(id string) (*models.User, error) {
+	sql := `
+select id, last_name, first_name
+from public.users
+where id = $1::uuid`
+	out := models.User{}
+	if err := s.DB.Get(&out, sql, id); err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
+
+func (s *UserService) Delete(id string) error {
+	sql := `
+delete from public.users
+where id = $1::uuid`
+	_, err := s.DB.Exec(sql, id)
+	return err
 }
--- internal/web/handlers/user.go
+++ internal/web/handlers/user.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/a-korkin/ecommerce/internal/core/models"
 	"github.com/a-korkin/ecommerce/internal/core/services"
+	"github.com/a-korkin/ecommerce/internal/utils"
 )
 
 type UserHandler struct {
@@ -22,7 +23,32 @@ func (u *UserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPost:
 		u.create(w, r)
 	case http.MethodGet:
-		u.getAll(w, r)
+		path := "/{id}"
+		vars := utils.GetVars(r.RequestURI, path)
+		id, ok := vars["id"]
+		if !ok {
+			u.getAll(w, r)
+			return
+		}
+		u.getByID(w, r, id)
+	case http.MethodPut:
+		path := "/{id}"
+		vars := utils.GetVars(r.RequestURI, path)
+		id, ok := vars["id"]
+		if !ok {
+			http.Error(w, "id not presented in url", http.StatusBadRequest)
+			return
+		}
+		u.update(w, r, id)
+	case http.MethodDelete:
+		path := "/{id}"
+		vars := utils.GetVars(r.RequestURI, path)
+		id, ok := vars["id"]
+		if !ok {
+			http.Error(w, "id not presented in url", http.StatusBadRequest)
+			return
+		}
+		u.delete(w, r, id)
 	}
 }
 
@@ -61,5 +87,51 @@ func (u *UserHandler) getAll(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, msg, http.StatusInternalServerError)
 		return
 	}
+}
 
+func (u *UserHandler) update(w http.ResponseWriter, r *http.Request, id string) {
+	in := models.UserIn{}
+	if err := json.NewDecoder(r.Body).Decode(&in); err != nil {
+		msg := fmt.Sprintf("failed to unmarshalling user: %s", err)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+	out, err := u.UserService.Update(id, &in)
+	if err != nil {
+		msg := fmt.Sprintf("failed to update user: %s", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(out); err != nil {
+		msg := fmt.Sprintf("failed to marshalling user: %s", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+}
+
+func (u *UserHandler) getByID(
+	w http.ResponseWriter, _ *http.Request, id string) {
+	out, err := u.UserService.GetByID(id)
+	if err != nil {
+		msg := fmt.Sprintf("failed to get user by id: %s", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(out); err != nil {
+		msg := fmt.Sprintf("failed to marshalling user: %s", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+}
+
+func (u *UserHandler) delete(
+	w http.ResponseWriter, _ *http.Request, id string) {
+	w.WriteHeader(http.StatusNoContent)
+	if err := u.UserService.Delete(id); err != nil {
+		msg := fmt.Sprintf("failed to delete user: %s", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
 }
